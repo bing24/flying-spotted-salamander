60aeed9051f552840bd9f08f6d6295e46c021feb
BarzinM <barzin.m@gmail.com>
2015-01-09 14:01:57 -0500

    added functions, TODO: debug

--- Matlab Simulation/Matlab_classes.m
+++ Matlab Simulation/Matlab_classes.m
@@ -6,16 +6,23 @@ classdef ChargingRobot
 		id
 		initial_x
 		initial_y
+		current_x
+		current_y
 		trajectory_x
 		trajectory_y
 		max_speed
 		power_level
 	end
-
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	methods
 		function obj=ChargingRobot()
 		end
-		function Move(obj, time_step)
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function setID(obj, ID)
+			obj.id=ID
+		end
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function move(obj, time_step)
 		end
 	end
 
@@ -28,6 +35,8 @@ classdef OperatingRobot
 		id
 		initial_x
 		initial_y
+		current_x
+		current_y
 		trajectory_x
 		trajectory_y
 		max_speed
@@ -36,11 +45,16 @@ classdef OperatingRobot
 		recharge_window_max_level
 		recharge_window_min_level
 	end
-
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	methods
 		function obj=OperatingRobot()
 		end
-		function Move(obj, time_step)
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function setID(obj, ID)
+			obj.id=ID
+		end
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function move(obj, time_step)
 		end
 	end
 
@@ -54,16 +68,29 @@ classdef Environment
 		list_of_operatoring_robots
 		list_of_charging_robots
 	end
-
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	methods
 		function obj=Environment()
 		end
-		function AddOperatingRobots(obj, number_of_robots_to_add)
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function addOperatingRobots(obj, number_of_robots_to_add)
 			for i=1:number_of_robots_to_add
+				list_of_operatoring_robots=[list_of_operatoring_robots, OperatingRobot()]
 			end
 		end
-		function AddChargingRobots(obj, number_of_robots_to_add)
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function addChargingRobots(obj, number_of_robots_to_add)
 			for i=1:number_of_robots_to_add
+				list_of_charging_robots=[list_of_charging_robots, OperatingRobot()]
+			end
+		end
+		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		function simStep(obj)
+			for i=1:length(list_of_operatoring_robots)
+				list_of_operatoring_robots(i).move(time_step)
+			end
+			for i=1:length(list_of_charging_robots)
+				list_of_charging_robots(i).move(time_step)
 			end
 		end
 	end
